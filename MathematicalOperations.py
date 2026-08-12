74ee7b2fc3425f65d2f12fcb962ccd6c42f9446e
Varad Paradkar <119121596+CodexxNinja@users.noreply.github.com>
2026-08-12 20:29:46 +0530

    Add files via upload

---
+++ MathematicalOperations.py
@@ -0,0 +1,13 @@
+# Addition of 2 Numbers
+
+n1 = int(input("Enter 1st Number: "))
+n2 = int(input("Enter 2nd Number: "))
+
+print("Addition is:",n1+n2)
+print("Subtraction is:",n1-n2)
+print("Multiplication is:",n1*n2)
+
+if n2==0:
+    print("Cannot Divide by Zero!")
+else:
+    print("Division is:",n1/n2)
