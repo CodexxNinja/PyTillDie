e696721c3b3ddafc60ede6ba162174f126a56cf2
Varad Paradkar <119121596+CodexxNinja@users.noreply.github.com>
2026-08-12 20:43:17 +0530

    Add files via upload

---
+++ VariableTypes.py
@@ -0,0 +1,21 @@
+# Checking types of variables in Python.
+
+#Creating Variables
+a = "Hi"
+b = 10
+c = 10.5
+d = True
+e = ["A","B","C","D"]
+f = ("A","B","C","D")
+g = {}
+
+#Checking Types
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+
+
