bdc82b7fd0a280b5fc3b4beff8a77689565a301c
Varad Paradkar <119121596+CodexxNinja@users.noreply.github.com>
2026-08-14 19:30:01 +0530

    Add files via upload

---
+++ SimpleInterest.py
@@ -0,0 +1,9 @@
+# Calculate simple interest
+
+p = int(input("Enter Principal amount: "))
+r = int(input("Enter Rate of Interest: "))
+t = int(input("Enter Time(in years): "))
+
+simple_interest = (p*r*t)/100
+
+print("Simple interest:",simple_interest)
